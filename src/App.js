f1c5eeccbe3f0da3f6436d19d119f2684b1996cf
Andrew Plank <aplank14@gmail.com>
2020-04-17 21:48:29 -0500

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,55 +1,29 @@
 import React from 'react'
 import './App.css'
-import {Link, BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Navbar, NavDropdown, Nav} from 'react-bootstrap'
 
-import Home from './components/Home'
 import Cities from './components/Cities'
+import City from './components/City'
 import Businesses from './components/Businesses'
 import Business from './components/Business'
-import City from './components/City'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
 
 function App() {
   return (
     <div>
       <Router>
-        <header>
-          <Navbar bg="light" expand="lg">
-            <Navbar.Brand>
-              <Link to="/">Helping Out Our Local Business</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link to="/">Home</Nav.Link>
-                <Nav.Link to="/">About Us</Nav.Link>
-                <NavDropdown title="Search" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="/business">By Business</NavDropdown.Item>
-                  <NavDropdown.Item href="/city">By City</NavDropdown.Item>
-
-                  {/* Split Drop Down
-         <NavDropdown.Divider />
-         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-         */}
-                </NavDropdown>
-              </Nav>
-
-              {/* Search Box
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
-  */}
-            </Navbar.Collapse>
-          </Navbar>
-
-          <Route exact path="/" component={Home} />
-          <Route exact path="/business" component={Businesses} />
-          <Route exact path="/city" component={Cities} />
+        <Header></Header>
+        <Switch>
           <Route path="/business/:id" component={Business} />
           <Route path="/city/:city" component={City} />
-        </header>
+          <Route path="/business" component={Businesses} />
+          <Route path="/city" component={Cities} />
+          <Route path="/" component={Home} />
+        </Switch>
+        <Footer></Footer>
       </Router>
     </div>
   )
---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {Navbar, NavDropdown, Nav} from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>
+          <Link to="/">Helping Out Our Local Business</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link to="/">Home</Nav.Link>
+            <Nav.Link to="/">About Us</Nav.Link>
+            <NavDropdown title="Search" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/business">By Business</NavDropdown.Item>
+              <NavDropdown.Item href="/city">By City</NavDropdown.Item>
+
+              {/* Split Drop Down
+   <NavDropdown.Divider />
+   <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+   */}
+            </NavDropdown>
+          </Nav>
+
+          {/* Search Box
+<Form inline>
+<FormControl type="text" placeholder="Search" className="mr-sm-2" />
+<Button variant="outline-success">Search</Button>
+</Form>
+*/}
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
