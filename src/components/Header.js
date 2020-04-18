62f32191e0d7822073142c7eebefb9a50a4e5d29
Lutreeno <39677032+Lutreeno@users.noreply.github.com>
2020-04-18 17:39:20 -0500

    Trying to get header to be White for text
    Stuck on Search

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,24 +5,27 @@ import {Navbar, NavDropdown, Nav} from 'react-bootstrap'
 const Header = () => {
   return (
     <header>
-      <Navbar bg="light" expand="lg">
+      <Navbar className="headerBackground" expand="lg">
         <Navbar.Brand>
           <Link to="/" className="linkOrange">Helping Out Our Local Business</Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link to="/" className="headerTitle">Home</Nav.Link>
-            <Nav.Link to="/">About Us</Nav.Link>
-            <NavDropdown title="Search" id="basic-nav-dropdown">
+            <Nav.Link to="/"><div className="headerTitle">Home</div></Nav.Link>
+            <Nav.Link to="/"><div className="headerTitle">About Us</div></Nav.Link>
+            <span className="headerTitle">
+            <NavDropdown style={{ tintColor: 'white' }} title="Search" id="basic-nav-dropdown">
+              
               <NavDropdown.Item href="/business">By Business</NavDropdown.Item>
-              <NavDropdown.Item href="/city">By City</NavDropdown.Item>
+              <NavDropdown.Item href="/city">By Location</NavDropdown.Item>
 
               {/* Split Drop Down
    <NavDropdown.Divider />
    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
    */}
             </NavDropdown>
+            </span>
           </Nav>
 
           {/* Search Box
