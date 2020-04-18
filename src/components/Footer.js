0da09b17dd0a90371572a7a4aabef2e433f436a2
Lutreeno <39677032+Lutreeno@users.noreply.github.com>
2020-04-18 09:45:53 -0500

    Footer Updates
    Updated Footer as a temporary placeholder for now. Still needs work done.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,78 @@
 import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
 
 const Footer = () => {
   return (
     <footer>
-        I am a footer
+        <div className="containerFull" style={{backgroundColor: "#333333"}}>
+          <Container className="footerPadding">
+            <br></br>
+              <Row>
+                <Col xs={12} md={3}>
+                  <div className="footerTitle">
+                    Company
+                  </div>
+                  <div className="footerBody">
+                    About
+                  </div>
+                  <div className="footerBody">
+                    Blog
+                  </div>
+                  <div className="footerBody">
+                    Careers
+                  </div>
+                </Col>
+
+                <Col xs={12} md={3}>
+                <div className="footerTitle">
+                    Company
+                  </div>
+                  <div className="footerBody">
+                    Test New Features
+                  </div>
+                  <div className="footerBody">
+                    Help Document
+                  </div>
+                  <div className="footerBody">
+                    Tools
+                  </div>
+                </Col>
+
+                <Col xs={12} md={3}>
+                <div className="footerTitle">
+                    Resources
+                  </div>
+                  <div className="footerBody">
+                    Privacy
+                  </div>
+                  <div className="footerBody">
+                    Contact
+                  </div>
+                  
+                </Col>
+                <Col xs={12} md={3}>
+                <div className="footerTitle">
+                    Product Help
+                  </div>
+                  <div className="footerBody">
+                    Support
+                  </div>
+                  <div className="footerBody">
+                    File a Bug
+                  </div>
+                  <div className="footerBody">
+                    Business Update
+                  </div>
+                </Col>
+
+              </Row>
+
+              <br></br>
+              <br></br>
+              <div align="center" className="footerCopyright">© 2020 STC</div>
+              <br></br>
+          </Container>
+        </div>
     </footer>
   )
 }
