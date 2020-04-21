4b12b9bdf899e5b2ad2a5d50e941ca3e04bb4acd
Andrew Plank <aplank14@gmail.com>
2020-04-21 17:14:57 -0500

    Fixed imports

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {Card} from 'react-bootstrap'
+import {Card, Col} from 'react-bootstrap'
 
 const CityCard = ({city}) => {
   return (
