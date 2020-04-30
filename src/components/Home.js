3d8099f1d8ce21cb3aec145e8a10326da26295fe
Lutreeno <39677032+Lutreeno@users.noreply.github.com>
2020-04-30 15:38:49 -0500

    Mobile Vs Desktop CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,11 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Video from './Video'
 import {Container, Row, Col} from 'react-bootstrap'
+import {Form, FormControl, Button} from 'react-bootstrap'
+
 import Shop_Small_Image from '../Photo/Shop_Small.jpg'
 import Support_Local from '../Photo/Support_Local.png'
+
 
 export default function Home() {
   /* Video for Home Page Background */
@@ -19,13 +22,42 @@ export default function Home() {
         </Col>
 
         <Col lg={6} md={12}>
-          <Container style = {{height:"calc(100vh - 65px)"}} className="containerFull justify-content-md-center">
-          <h3>Find Local Treasures <br />you'll love
-            
-          </h3>
+          <Container style = {{height:"calc(100vh - 65px)"}} className="aligncenter">
+          <h2>Support each other with your Local Option
+          </h2>
+          <br />
+          <Form inline>
+              <FormControl type="text" placeholder="City, State or Zipcode" className="mr-sm-2" />
+              <Button variant="outline-dark">Search</Button> <Button variant="outline-primary"> Near Me</Button>
+            </Form>
           </Container>
         </Col>
-      </Row>
+        </Row>
+
+
+{/* Need to add pagging */}
+        <br /><br /><br />
+
+<Container style={{height:"90vw"}}>
+  <Row>
+    <Col lg={4} md={4} sm={10} className="text-center">
+      <h3>Local Treasures</h3>
+      Find the hidden gems inside any city. From antique shops to restaurants, find what you are looking for
+    </Col>
+
+    <Col lg={4} md={4} sm={10} className="text-center">
+      <h3>Supporting the Community</h3>
+      We believe buying locally is the best way to support the local community. A stronger local community means a better quality of life for all.
+    </Col>
+
+    <Col lg={4} md={4} sm={10} className="text-center">
+      <h3>Reinvest Cycle</h3>
+      From sponsoring little leagues to funding public events for all, our local options give back. Investing in them, is investing in so much more.
+    </Col>
+</Row>
+</Container>
+        <br />
+      
 
 
       <h1 align="center" className="titleMargin">
