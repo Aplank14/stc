eb2a9cbcbeb876f7df50da424cde76ce01d0451f
Aplank14 <aplank14@gmail.com>
2020-05-16 17:51:37 -0400

    Added dynamic state links to map

--- src/components/State.js
+++ src/components/State.js
@@ -3,6 +3,7 @@ import {Container, Row, Col} from 'react-bootstrap'
 import {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
 import CityCard from './CityCard.js'
+import states from '../utils/states.json'
 
 export default function State() {
   const [loading, setLoading] = useState(true)
@@ -12,59 +13,6 @@ export default function State() {
   let {id} = useParams()
 
   useEffect(() => {
-    const states = {
-      "AL": "Alabama",
-      "AK": "Alaska",
-      "AZ": "Arizona",
-      "AR": "Arkansas",
-      "CA": "California",
-      "CO": "Colorado",
-      "CT": "Connecticut",
-      "DE": "Delaware",
-      "FL": "Florida",
-      "GA": "Georgia",
-      "HI": "Hawaii",
-      "ID": "Idaho",
-      "IL": "Illinois",
-      "IN": "Indiana",
-      "IA": "Iowa",
-      "KS": "Kansas",
-      "KY": "Kentucky",
-      "LA": "Louisiana",
-      "ME": "Maine",
-      "MD": "Maryland",
-      "MA": "Massachusetts",
-      "MI": "Michigan",
-      "MN": "Minnesota",
-      "MS": "Mississippi",
-      "MO": "Missouri",
-      "MT": "Montana",
-      "NE": "Nebraska",
-      "NV": "Nevada",
-      "NH": "New Hampshire",
-      "NJ": "New Jersey",
-      "NM": "New Mexico",
-      "NY": "New York",
-      "NC": "North Carolina",
-      "ND": "North Dakota",
-      "OH": "Ohio",
-      "OK": "Oklahoma",
-      "OR": "Oregon",
-      "PA": "Pennsylvania",
-      "RI": "Rhode Island",
-      "SC": "South Carolina",
-      "SD": "South Dakota",
-      "TN": "Tennessee",
-      "TX": "Texas",
-      "UT": "Utah",
-      "VT": "Vermont",
-      "VA": "Virginia",
-      "WA": "Washington",
-      "WV": "West Virginia",
-      "WI": "Wisconsin",
-      "WY": "Wyoming"
-    }
-
     async function fetchData() {
       if(!states.hasOwnProperty(id)){
         return
