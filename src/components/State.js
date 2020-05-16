9d3a13f9362385df6d962cdb7e6f32653dc1c15c
Aplank14 <aplank14@gmail.com>
2020-05-16 17:05:43 -0400

    Fixed state abbreviation

--- src/components/State.js
+++ src/components/State.js
@@ -7,11 +7,78 @@ import CityCard from './CityCard.js'
 export default function State() {
   const [loading, setLoading] = useState(true)
   const [cities, setCities] = useState([])
+  const [stateName, setStateName] = useState([])
+
+  const states = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AS": "American Samoa",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "DC": "District Of Columbia",
+    "FM": "Federated States Of Micronesia",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "GU": "Guam",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MH": "Marshall Islands",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "MP": "Northern Mariana Islands",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PW": "Palau",
+    "PA": "Pennsylvania",
+    "PR": "Puerto Rico",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VI": "Virgin Islands",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming"
+  }
 
   let {id} = useParams()
 
   useEffect(() => {
     async function fetchData() {
+      if(!states.hasOwnProperty(id)){
+        return
+      }
+      setStateName(states[id])
       const url = `${process.env.REACT_APP_API_URL}/state/${id}`
       const response = await fetch(url)
       if (response.status!==200) {
@@ -40,7 +107,7 @@ export default function State() {
   return (
     <div>
       <Container className="containerFull justify-content-md-center text-center">
-        <h1 className="titleMargin">{cities.State} Cities</h1>
+        <h1 className="titleMargin">{stateName} Cities</h1>
         <Row className="">{cityPages}</Row>
       </Container>
       <br></br>
